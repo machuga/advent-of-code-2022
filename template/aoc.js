b25e4eeafc393a440a10b95f6617d92874a84879
Matt Machuga <machuga@gmail.com>
2022-12-03 09:26:46 -0500

    Adding template

---
+++ template/aoc.js
@@ -0,0 +1,20 @@
+const { parseRawInput } = require('../util.js');
+const [filename = 0] = process.argv.slice(2);
+const inputList = parseRawInput(filename).trim();
+
+const part1 = () => {
+  const compute = () => {
+  };
+
+  console.log(`Part 1: Count is "${compute()}"`);
+};
+
+const part2 = () => {
+  const compute = () => {
+  };
+
+  console.log(`Part 2: Count is "${compute()}"`);
+};
+
+part1();
+part2();
