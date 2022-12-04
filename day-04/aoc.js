313f545f831965b5db2078598690e66c090e683c
Matt Machuga <machuga@gmail.com>
2022-12-04 07:35:10 -0500

    Adding day 4 part 2

--- day-04/aoc.js
+++ day-04/aoc.js
@@ -3,8 +3,7 @@ const [arg = 0] = process.argv.slice(2);
 const filename = processInputArg(arg);
 const inputList = parseRawInput(filename).trim();
 
-const part1 = () => {
-  const parseToRanges = pipe([
+const parseToRanges = pipe([
     split("\n"),
     map(pipe([
       split(","),
@@ -17,6 +16,7 @@ const part1 = () => {
     ])),
   ]);
 
+const part1 = () => {
   const containsPairSet = ([a,b]) => doesSetContain(a,b) || doesSetContain(b,a)
 
   const doesSetContain = ([minA, maxA], [minB, maxB]) => (minA <= minB && maxA >= maxB)
@@ -34,7 +34,19 @@ const part1 = () => {
 };
 
 const part2 = () => {
+  const containsPairSet = ([a,b]) =>
+    numberInRange(a[0], b) || numberInRange(a[1], b) || numberInRange(b[0], a) || numberInRange(b[1], a)
+
+  const numberInRange = (num, [min, max]) => 
+    (min <= num && max >= num)
+
   const compute = () => {
+
+    return pipe([
+      parseToRanges,
+      map(containsPairSet),
+      filter(Boolean)
+    ])(inputList).length;
   };
 
   console.log(`Part 2: Count is "${compute()}"`);
