38ca811f777c63370d02491b2802f09c3563a304
Matt Machuga <machuga@gmail.com>
2023-10-15 07:55:54 -0400

    Day 10 part 2

--- day-10/aoc.ts
+++ day-10/aoc.ts
@@ -1,6 +1,4 @@
 import {
-  join,
-  last,
   sum,
   map,
   parseRawInput,
@@ -8,54 +6,35 @@ import {
   processInputArg,
   reduce,
   split,
+  chunk,
   tap,
-  toInt,
-  zip,
 } from "../util.ts";
 
 const [arg] = Deno.args;
 const filename = processInputArg(arg);
-console.log("Reading from", filename)
 const inputList = parseRawInput(filename).trim();
-
-type Operation = 'addx' | 'noop';
-type NoOp = ['noop'];
-type Instruction = [Operation, number] | NoOp;
 
 const parseInstructions = pipe([
   split("\n"),
   map(pipe([
     split(" "),
-    tap("Split"),
     ([op, arg]) : number => op == 'noop' ? 0 : parseInt(arg, 10),
   ])),
-  reduce((acc, e) => e == 0 ? acc.concat(e) : acc.concat([0, e]), [0])
-  // Note: Padded the above by 1 so that I don't have to do cycle + 1
+  reduce((acc, e) => e == 0 ? acc.concat(e) : acc.concat([0, e]), [0]),
 ]);
-
-//const padInstructions = (instructions : Instruction[]) => instructions.concat([['noop'], ['noop']]);
 
 const part1 = () => {
   const compute = () => {
     return pipe([
       parseInstructions,
-      tap("Instructions"),
-      //(list) => [1].concat(list),
-      //accumulate,
-      tap("Values are"),
-      //padInstructions,
-      tap("Starting cycles"),
       reduce(([x, tracked], num, cycle) => {
-        console.log("On cycle", cycle, "X is", x);
 
         if (cycle % 40 == 20) {
-          console.log("Pushing...", cycle, x, cycle * x);
           tracked.push(cycle * x);
         }
 
         return [x + num, tracked];
       }, [1, []]),
-      tap("Here we go"),
       ([x, tracked]) => sum(tracked),
     ])(inputList);
   };
@@ -65,11 +44,24 @@ const part1 = () => {
 
 const part2 = () => {
   const compute = () => {
+    const cursorOffsets = [-1, 0, 1];
+
     return pipe([
       parseInstructions,
+      reduce(([x, tracked], num, cycle) => {
+        const relevantCycle = (cycle - 1) % 40 - x;
+        const pixel = cursorOffsets.includes(relevantCycle) ? '#' : '.';
+
+        tracked.push(pixel);
+
+        return [x + num, tracked];
+      }, [1, []]),
+      ([x, tracked]) => tracked,
+      chunk(40),
+      map((row) => row.join('')),
     ])(inputList);
   };
-  //console.log(`Part 2: Count is "${compute()}"`);
+  console.log(`Part 2: Count is "`, compute());
 };
 
 part1();
